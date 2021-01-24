be35f0d9bf09be1e2682904b8937f57723dcdc1d
stevearchuleta <stevearchuleta@me.com>
2021-01-24 10:59:42 -0800

    complete express.js server-side web app as the foundation for my API development

--- src/index.js
+++ src/index.js
@@ -12,14 +12,15 @@ const express = require('express'); //request express dependency
 const app = express(); //create app object
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {res.send('JavaScript Everywhere')}); //call the app object's get method such that when a user accessess the root URL (/), my server sends a reponse of 'JavaScript Everywhere'
+app.get('/', (req, res) => {res.send('JavaScript Everywhere: A notedly app by Steve Archuleta')}); //call the app object's get method such that when a user accessess the root URL (/), my server sends a reponse of 'JavaScript Everywhere'
 app.listen(PORT, () => {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`); //template leteral syntax
 }); //make my app locally available on port 400 http://localhost:4000
 
 /*
 NOTE:   do not forget to run this app before accessing localhost:4000
-NOTE:   in terminal:  from the api-master directory, node src/index.js
+NOTE:   in terminal:  from the api-master directory, node src/index.js; ctrl c to cancel process
+NOTE:   or... even better... npm run dev (will enact the nodemon script in package.json)...
 */
 
 
