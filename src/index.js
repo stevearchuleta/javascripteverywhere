10ac96a7f9be127e7316ccf4fccdd9f0dc1e547c
stevearchuleta <stevearchuleta@me.com>
2021-01-24 17:58:21 -0800

    set up a basic GraphQL application with a schema and resolver

--- src/index.js
+++ src/index.js
@@ -1,27 +1,85 @@
-/* 
-index.js 
-This is the main entry point of my notedly application 
-*/
+// =========================
+// index.js 
+// This is the main entry point of my notedly application 
+// =========================
 
-/*
-Express.js Framework 
-To build a server-side web app that will serve as the basis for the backend of my API
-*/
 
-const express = require('express'); //request express dependency
-const app = express(); //create app object
+// =========================
+// Express.js Framework 
+// To build a server-side web app that will serve as the basis for the backend of my API
+// =========================
+
+
+// =========================
+// Pass express dependency into the Node.js require method
+// =========================
+const express = require('express');
+
+
+// =========================
+// Pass apollo-server-express dependency to the Node.js require method; allows me to serve data as a GraphQL API
+// =========================
+const { ApolloServer, gql } = require('apollo-server-express');
+
+
+// =========================
+// Run my server on a port that is either specified in my .env file or port 4000
+// Dynamically sets the port in the Node .env environment or port 4000 when no port is specified
+// =========================
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {res.send('JavaScript Everywhere: A notedly app by Steve Archuleta')}); //call the app object's get method such that when a user accessess the root URL (/), my server sends a reponse of 'JavaScript Everywhere'
+
+// =========================
+// GraphQL application schema
+// This is GraphQL schema language
+// =========================
+const typeDefs = gql `
+type Query {
+    hello: String
+}
+`;
+
+
+// =========================
+// A necessary GraphQL resolver function for my schema fields; resolvers return a value to the user
+// =========================
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+};
+
+
+// =========================
+// Create an app object
+// =========================
+const app = express();
+
+
+// =========================
+// Apollo Server Setup
+// =========================
+const server = new ApolloServer({ typeDefs, resolvers });
+
+
+// =========================
+//  Apply GraphQL Middleware (...and set path to /api)
+// =========================
+server.applyMiddleware({ app, path: '/api' });
+
+
+// =========================
+// Make my app locally available on port 400 http://localhost:4000
+// Template leteral syntax
+// =========================
 app.listen(PORT, () => {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`); //template leteral syntax
-}); //make my app locally available on port 400 http://localhost:4000
+    console.log(
+        `🌎  ==> GraphQL Server (The GraphQL Playground) is now running at http://localhost:${PORT}${server.graphqlPath}`
+    ) 
+}); 
 
 /*
-NOTE:   do not forget to run this app before accessing localhost:4000
+NOTE:   do not forget to 'run' this app in the terminal before accessing localhost:4000
 NOTE:   in terminal:  from the api-master directory, node src/index.js; ctrl c to cancel process
-NOTE:   or... even better... npm run dev (will enact the nodemon script in package.json)...
+NOTE:   or... even better... npm run dev (this will enact the nodemon script in package.json)
 */
-
-
-
