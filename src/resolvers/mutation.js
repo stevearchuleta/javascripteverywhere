47aba0f3f44f15d4787c31a08a783dbc251e9ed6
stevearchuleta <stevearchuleta@me.com>
2021-01-30 09:59:55 -0800

    create a JWT_SECRET variable in the .env file; import into src/resolvers/mutation the required packages in order to write my resolver signUp mutation; write a signUp mutation in src/resolvers/mutation.js

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -3,6 +3,17 @@
 // Resolver Functions MongoDB model's create() method, find() method, findById() method 
 // =========================
 
+
+// =========================
+// Import all of the requred packages necessary to write my signUp mutation
+// =========================
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+
+
 module.exports = {
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
@@ -32,5 +43,45 @@ module.exports = {
                 new: true
             }
         )
+    },
+    signUp: async(parent, { username, email, password }, { models }) => {
+
+        // =========================
+        // Normalize email address
+        // =========================
+        email = email.trim().toLowerCase();
+
+        // =========================
+        // Hash the password
+        // =========================
+        const hashed = await bcrypt.hash(password, 10);
+
+        // =========================
+        // Create the gravatar url
+        // =========================
+        const avatar = gravatar(email);
+        
+        try {
+            const user = await models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+
+        // =========================
+        // Create and return the JsonWebToken
+        // =========================
+        return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        
+        } catch (err) {
+          console.error(err);
+
+        // =========================
+        // If there is a problem creating the account, throw an error
+        // =========================
+        throw new Error('There was an error creating this account.')
+
+        }
     }
 } 
