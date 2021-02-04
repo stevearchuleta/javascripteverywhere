677e2c8d96087f04eb6737775457375fb63337d2
stevearchuleta <stevearchuleta@me.com>
2021-02-04 11:13:37 -0800

    write noteFeed resolver code in src/resolvers/query.js; page 85-86 JS Everywhere

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -21,5 +21,63 @@ module.exports = {
     },
     me: async (parent, args, { models, user }) => {
         return await models.User.findById(user.id)
+    },
+    noteFeed: async (parent, { cursor }, { models }) => {
+
+
+        // =========================
+        // Hardcode the cursor-based pagination limit to 10 items
+        // =========================
+        const limit = 10;
+
+
+        // =========================
+        // Set the default hasNextPage value to false
+        // =========================
+        let hasNextPage = false;
+
+
+        // =========================
+        // If no cursor is passed as an argument, then the default query will remain empty; this will pull the newest notes from the database
+        // =========================
+        let cursorQuery = {};
+
+
+        // =========================
+        // If a cursor is passed as an argument, my query will search for notes with an ObjectId less that that of the cursor
+        // lt means less than, in the MongoDB query language
+        // =========================
+        if (cursor) {
+            cursorQuery = { _id: { $lt: cursor } }
+        }
+
+
+        // =========================
+        // Find the 'limit plus one' of notes in my database and sort newest to oldest
+        // =========================
+        let notes = await models.Note.find(cursorQuery).sort({ _id: -1 }).limit(limit + 1);
+
+
+        // =========================
+        // If the number of notes I find exceeds my limit
+        // =========================
+        if ( notes.length > limit ) {
+            hasNextPage = true;
+            notes = notes.slice(0, -1);
+        }
+
+
+        // =========================
+        // The new cursor will be the Mongo Object ID of the last item in the feed array
+        // recall: 0th index = notes[0]; last index = notes[notes.length - 1]
+        // =========================
+        const newCursor = notes[notes.length - 1]._id;
+
+
+        return {
+            notes, 
+            cursor: newCursor, 
+            hasNextPage
+        }
     }
 }
